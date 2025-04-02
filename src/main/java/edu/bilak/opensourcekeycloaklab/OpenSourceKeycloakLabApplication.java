3af8a3f06060a7c19bdf3eb6499eb656fd988ac2
Yuliana Bilak <bilakyuliana6996@gmail.com>
2025-04-02 19:46:44 +0300

    initial commit

---
+++ src/main/java/edu/bilak/opensourcekeycloaklab/OpenSourceKeycloakLabApplication.java
@@ -0,0 +1,13 @@
+package edu.bilak.opensourcekeycloaklab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSourceKeycloakLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenSourceKeycloakLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/bilak/opensourcekeycloaklab/OpenSourceKeycloakLabApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bilak.opensourcekeycloaklab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSourceKeycloakLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
